746b6467824768332c81153ca7e7337068f7a70c
Tobias Gaertner <tga@xima.de>
2026-08-31 14:55:04 +0200

    [TASK] Remove `sys_refindex` from `ignore_tables_out`
    The table needs to be up to date and in sync with other datebase tabels. Reference Index update is more expensive then just including this table.

--- deployer/default/set.php
+++ deployer/default/set.php
@@ -30,7 +30,6 @@ set('db_default', [
         'fe_session_data',
         'sys_file_processedfile',
         'sys_http_report',
-        'sys_refindex',
         'tx_devlog',
         'tx_extensionmanager_domain_model_extension',
         'tx_solr_.*',
